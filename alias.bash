aa47b029d81171c49cbf016c5f55a55b5d484ce2
Your Name <you@example.com>
2016-11-17 00:21:44 -0600

    xterm

--- alias.bash
+++ alias.bash
@@ -1,4 +1,9 @@
 # pip install http://bitbucket.org/sjl/t/get/tip.zip
 
+if [ -e /usr/share/terminfo/x/xterm-256color ]; then
+    export TERM='xterm-256color'
+else
+    export TERM='xterm-color'
+fi
 alias t='t --task-dir ~/Dropbox/tasks --list tasks'
 
