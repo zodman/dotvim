d97ea0fc68516e1ae468e848082971e9395aab82
Andres Vargas <andres@python3.ninja>
2021-05-27 15:55:30 -0500

    small change

--- alias.bash
+++ alias.bash
@@ -85,9 +85,11 @@ function gitignore() { curl -sL https://www.toptal.com/developers/gitignore/api/
 function aws-echo-keys() {
     AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id`
     AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key`
+    AWS_REGION=`aws configure get region`
     echo "# AWS_PROFILE=${AWS_PROFILE}"
     echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}"
     echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
+    echo "AWS_REGION=${AWS_REGION}"
 }
 
 
