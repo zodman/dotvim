b910a1c727ec659d9208a3b3fead18d570f3a661
zodman <zodman@gmail.com>
2023-07-13 11:52:30 -0400

    update script

--- bin/install-from-github.sh
+++ bin/install-from-github.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# requirements: 
+# requirements:
 # https://github.com/dvershinin/lastversion
 # https://github.com/charmbracelet/gum
 ### used lastversion and gum
@@ -7,25 +7,31 @@
 set -o errexit
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-    echo "Usage: ./$0 <software>"
-    exit
+	echo "Usage: ./$0 <software>"
+	exit
 fi
 
-selected=$(gum  choose `lastversion --assets $1`)
-tmpfile=$(mktemp /tmp/XXXX.deb)
+selected=$(gum choose $(lastversion --assets $1))
+
+tmp_filename=$(curl -s -q -L -I $selected | grep 'disposition' | tail -n1 | awk '{print $3}' | sed s/filename=//g | sed 's/\r$//g')
+
+tmpfile=$(mktemp /tmp/XXXX.$tmp_filename)
+
 gum spin --title="Downloading $selected" -- curl -s -q -L $selected -o $tmpfile
 
 red "Install the $tmpfile"
-if [[ $tmpfile == *.deb ]] 
-then
-    sudo  dpkg -i  $tmpfile
-elif [[ $tmpfile == *.zip ]]
-then
-    sudo unzip -d /usr/local/ $tmpfile
+if [[ $tmpfile == *.deb ]]; then
+	sudo dpkg -i $tmpfile
+elif [[ $tmpfile == *.tar.gz ]]; then
+	ouput_dir="/usr/local/src/$tmp_filename"
+	sudo mkdir -p $ouput_dir
+	sudo tar -zxvf $tmpfile --directory $ouput_dir
+	red "installed at $ouput_dir"
+elif [[ $tmpfile == *.zip ]]; then
+	sudo unzip -d /usr/local/ $tmpfile
 else
-    red "No installed the $tmpfile"
-    
+	red "No installed the $tmpfile"
+
 fi
 
 rm -rf $tmpfile
-
