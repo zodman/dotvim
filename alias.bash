dd5a479a0b404987b494b932608d026b90f479bc
Andres Vargas <avargas@nicoyalife.com>
2023-09-15 11:05:57 -0400

    update changes

--- alias.bash
+++ alias.bash
@@ -325,6 +325,10 @@ git-sync-main-dev() {
 	red done
 }
 
+srctodo() {
+	rg -i TODO --vimgrep | awk '{split($1,arr,":"); print "\"git blame -f -n -L"  arr[2] "," arr[2], arr[1] "\""}' | xargs -n1 bash -c | rg "$(git config --global user.name)"
+}
+
 ##### ALIAS
 alias m=anybar_monitor
 alias wttr='curl wttr.in -L'
