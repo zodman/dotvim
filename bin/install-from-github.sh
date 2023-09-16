96925b70822a0d47448f3a2fc93d84fcfcea00cd
Andres Vargas <avargas@nicoyalife.com>
2023-09-16 19:37:16 -0400

    Update install-from-github script and jira-list sorting

--- bin/install-from-github.sh
+++ bin/install-from-github.sh
@@ -21,13 +21,18 @@ selected=$(gum choose $(lastversion --assets $1))
 
 tmp_filename=$(curl -s -q -L -I $selected | grep 'disposition' | tail -n1 | awk '{print $3}' | sed s/filename=//g | sed 's/\r$//g')
 
-tmpfile=$(mktemp /tmp/XXXX.$tmp_filename)
+#tmpfile=$(mktemp /tmp/XXXX.$tmp_filename)
+tmpfile=/tmp/$tmp_filename
 
 gum spin --title="Downloading $selected" -- curl -s -q -L $selected -o $tmpfile
 
 red "Install the $tmpfile"
 if [[ $tmpfile == *.deb ]]; then
 	sudo dpkg -i $tmpfile
+elif [[ $tmpfile == *.appimage ]]; then
+	sudo install $tmpfile $HOME/.local/bin/
+	sudo ln -s $HOME/.local/bin/$tmp_filename \
+		$HOME/.local/bin/$(echo $tmp_filename | sed s/\.appimage//g)
 elif [[ $tmpfile == *.tar.gz ]]; then
 	ouput_dir="/usr/local/src/$tmp_filename"
 	sudo mkdir -p $ouput_dir
