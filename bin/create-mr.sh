f0d363b07c63b78555efc2cda162301a2083b8f2
Andres Vargas <avargas@nicoyalife.com>
2024-04-19 16:50:46 -0400

    last update

--- bin/create-mr.sh
+++ bin/create-mr.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 __create_pr() {
+
 	BRANCH=$(git rev-parse --abbrev-ref HEAD | grep -oE '[A-Z]{2,9}-[0-9]{4}')
 	TITLE=$(jira-issue $BRANCH | jq -r '.summary')
 	KEY=$(jira-issue $BRANCH | jq -r '.key')
@@ -15,8 +16,19 @@ $TITLE
 $SUMMARY
 EOF
 	)
-	BRANCH_TARGET=$(gum filter $(git for-each-ref --format="%(refname:short)" refs/heads/))
-	glab mr create -a avargas101 --title "[$KEY] $TITLE" --description "$BODY" --draft --target-branch $BRANCH_TARGET -l $BRANCH_TARGET
+
+	BRANCH_TARGET=$(git for-each-ref --format="%(refname:short)" \
+		refs/heads/ | gum filter)
+	glab mr create -a avargas101 --title "[$KEY] $TITLE" \
+		--description "$BODY" --draft \
+		--target-branch $BRANCH_TARGET -l $BRANCH_TARGET
+
+	web_url=$(glab api merge_request?source_branch=$BRANCH 2>/dev/null |
+		jq -r '.[].web_url' | head -1)
+	jira-add-link $BRANCH $web_url "Gitlab MR"
+
+
+"
 
 }
 
--- bin/edit-mr.sh
+++ bin/edit-mr.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
+set -x
 BRANCH=$(echo $1 | grep -oE '[A-Z]{2,9}-[0-9]{4}')
 TITLE=$(jira-issue $BRANCH | jq -r '.summary')
 KEY=$(jira-issue $BRANCH | jq -r '.key')
@@ -16,3 +17,7 @@ EOF
 )
 
 glab mr update $1 --title "[$KEY] $TITLE" --description "$BODY"
+
+web_url=$(glab api merge_request?source_branch=$BRANCH 2>/dev/null |
+	jq -r '.[].web_url' | head -1)
+jira-add-link $BRANCH $web_url "Gitlab MR 👨💻"
