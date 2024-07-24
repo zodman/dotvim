3acc38a553fe1ced60a2aabe98d1c66395036671
Andres Vargas <avargas@nicoyalife.com>
2024-07-24 17:06:43 -0400

    Update JIRA link with correct branch ID in create-mr script

--- bin/create-mr.sh
+++ bin/create-mr.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 __create_pr() {
-
-	BRANCH=$(git rev-parse --abbrev-ref HEAD | grep -oE '[A-Z]{2,9}-[0-9]{4}')
-	TITLE=$(jira-issue $BRANCH | jq -r '.summary')
-	KEY=$(jira-issue $BRANCH | jq -r '.key')
-	LINK=$(jira-issue $BRANCH | jq -r '.link')
-	SUMMARY=$(jira-issue $BRANCH | jq -r '.description')
+	BRANCH=$(git rev-parse --abbrev-ref HEAD)
+	JIRA_ID=$(echo $BRANCH | grep -oE '[A-Z]{2,9}-[0-9]{4}')
+	TITLE=$(jira-issue $JIRA_ID | jq -r '.summary')
+	KEY=$(jira-issue $JIRA_ID | jq -r '.key')
+	LINK=$(jira-issue $JIRA_ID | jq -r '.link')
+	SUMMARY=$(jira-issue $JIRA_ID | jq -r '.description')
 	REPO_NAME=$(glab repo view -F json | jq -r '.name')
 	BODY=$(
 		cat <<EOF
@@ -27,7 +27,7 @@ EOF
 	web_url=$(glab api merge_requests?source_branch=$BRANCH 2>/dev/null |
 		jq -r '.[].web_url' | head -1)
 	set -x
-	jira-add-link "$BRANCH" "$web_url" "Gitlab MR on ${REPO_NAME}"
+	jira-add-link "$JIRA_ID" "$web_url" "Gitlab MR on ${REPO_NAME}"
 	set +x
 }
 
