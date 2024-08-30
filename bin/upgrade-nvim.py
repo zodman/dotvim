29470defccc40233b1f4720a319428755ada5e20
Andres Vargas <avargas@nicoyalife.com>
2024-08-29 22:56:36 -0400

    Add new scripts and color schemes for Tokyo Night theme

--- bin/upgrade-nvim.py
+++ bin/upgrade-nvim.py
@@ -8,7 +8,7 @@ HOME = os.getenv("HOME")
 os.chdir(os.path.join(HOME, '.local', 'bin'))
 
 ~"rm -f nvim.appimage"
-~"curl -s -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
+~"curl -s -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
 ~"chmod u+x nvim.appimage"
 print(f'{colorama.Fore.GREEN}> Install success {colorama.Fore.RESET}')
-~"./nvim.appimage --version"
+~"nvim.appimage --version"
